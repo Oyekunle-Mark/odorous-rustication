a0f03257ed1e5faa69844320dc9e697043e7eda9
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-19 19:50:32 +0100

    Converts between fahrenheit and celcius

--- fahrenheit_celsius_converter/src/main.rs
+++ fahrenheit_celsius_converter/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("This program prints the Celcius equivalent of temperatures entered in Fahrenheit.");
+
+    loop {
+        println!("Enter temperature in Fahrenheit");
+        println!("Enter zero to terminate the program");
+
+        let mut temp = String::new();
+
+        io::stdin()
+            .read_line(&mut temp)
+            .expect("Could not read line");
+
+        let temp = temp.trim().parse::<f64>().unwrap();
+
+        if temp == 0.0 {
+            break;
+        }
+
+        let fahr = (temp - 32.0) * 5.0 / 9.0;
+        println!(
+            "The Celcius equivalent of {} Fahrenheit is {} Celcius",
+            fahr, temp
+        );
+    }
+
+    println!("Thank you!");
 }
