af4bc28f71d2d0c26bb134468276ceedb425ebb3
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-16 17:31:33 +0100

    Refactored with struct

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,9 +1,20 @@
-fn main() {
-    let rect = (50, 60);
-
-    println!("The area of the rectangle is {} square pixels.", area(rect));
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(rect: (u32, u32)) -> u32 {
-    rect.0 * rect.1
+fn main() {
+    let rect = Rectangle {
+        width: 50,
+        height: 60,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect)
+    );
+}
+
+fn area(dimension: &Rectangle) -> u32 {
+    dimension.width * dimension.height
 }
