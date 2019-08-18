1d4fbce8c4b2ac710c0e53cbe40fb2bead762914
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-18 16:29:26 +0100

    Executed a block on a match arm

--- enum_and_match/src/main.rs
+++ enum_and_match/src/main.rs
@@ -7,7 +7,10 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> i32 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
@@ -15,7 +18,7 @@ fn value_in_cents(coin: Coin) -> i32 {
 }
 
 fn main() {
-    let nickel = Coin::Nickel;
+    let penny = Coin::Penny;
 
-    println!("The value of nickel is {}!", value_in_cents(nickel));
+    println!("The value of penny is {}!", value_in_cents(penny));
 }
